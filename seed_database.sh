418082c68ae2f4470de11c45d160b0e640aae856
Sarah Marie Shepherd <sarahmarieshep@gmail.com>
2024-07-26 13:09:18 -0400

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./movieapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations movieapi
+python3 manage.py migrate movieapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
