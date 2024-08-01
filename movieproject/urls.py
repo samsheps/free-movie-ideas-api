2f9c7db7ab0b52290dc39b0b169b2c4bfc3f3260
Sarah Marie Shepherd <sarahmarieshep@gmail.com>
2024-08-01 18:31:01 -0400

    added update, delete, mark as watched and add to watchlist features

--- movieproject/urls.py
+++ movieproject/urls.py
@@ -11,7 +11,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from movieapi.views import register_user, login_user, MovieView, UserView, UserMovieView, GenreView
+from movieapi.views import register_user, login_user, MovieView, UserView, UserMovieView, GenreView, LikedMovieView, MarkAsWatchedView
 from django.conf.urls import include
 
 
@@ -21,10 +21,13 @@ router.register(r'movies', MovieView, basename='movie')
 router.register(r'users', UserView, basename='user')
 router.register(r'usermovies', UserMovieView, basename='usermovie')
 router.register(r'genres', GenreView, basename='genre')
+router.register(r'likedmovies', LikedMovieView, basename='likedmovie')
+# router.register(r'watched', MarkAsWatchedView, basename="markwatched")
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('likedmovies/<int:movie_id>/mark_watched/', MarkAsWatchedView.as_view(), name='mark-watched'),
 ]
